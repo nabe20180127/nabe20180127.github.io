3aa67203b68f062f3b9c101f0c273aa060f9d679
nabe20180127 <35859773+nabe20180127@users.noreply.github.com>
2019-12-07 04:38:42 +0900

    moved!!

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+
+let count = 0
+const Btn = document.getElementById('btn')
+const Now = document.getElementById('tim')
+
+const init = () => {
+  count = 0
+  Btn.innerText = count
+
+}
+init()
+
+Btn.addEventListener('click', () => {
+  count++
+  Btn.innerText = count
+})
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js')
     .then(function() {
       console.log('Service worker registered!');
     });
 }
+
--- sw.js
+++ sw.js
@@ -1,17 +1,1 @@
-let count = 0
-const Btn = document.getElementById('btn')
-const Now = document.getElementById('tim')
-
-const init = () => {
-  count = 0
-  Btn.innerText = count
-
-}
-init()
-
-Btn.addEventListener('click', () => {
-  count++
-  Btn.innerText = count
-})
-
 
